4e70194df5af8390ec08e435cfa86891c2b11568
dragonGTR <77241091+dragonGTR@users.noreply.github.com>
2021-07-01 11:00:42 +0530

    Update urls.py

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main', views.index, name="index"),
-    path('allUser', views.display_user, name="index2")
+    path('main', views.index, name="index")
 ]
